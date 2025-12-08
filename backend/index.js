608532cc8b052a2d6a4c2420e3b8f8198fff08bc
dariaskurtu <dashaskurtu@icloud.com>
2025-12-08 08:47:43 +0000

    signin

---
+++ backend/index.js
@@ -0,0 +1,68 @@
+const cookieParser = require("cookie-parser")
+const db = require("./db")
+const express = require("express")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const session = require("express-session")
+
+const app = express()
+exports.app = app
+
+app.set("trust proxy", 1) // Только для codespace
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: true, // Только для codespace
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["set-cookie"]
+}))
+
+app.use(session({
+    secret: "dpskdosk",
+    name: "sessionId",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        //sameSite: "strict",
+        sameSite: "none", // Только для codespace
+        secure: true, // fals if localhost\
+        domain: undefined //Только для codespace
+    }
+}))
+
+app.get("/auth/me", (req, res) => {
+    console.log(req.session)
+    if (req.session.userId) {
+        return res.json({ loggedIn: true})
+    }
+})
+app.post("/auth/signup", (req, res) => {
+    try {
+        const hashed = bcrypt.hashSync(req.body.password, 10)
+        const newUser = db.prepare(`INSERT INTO users (email, password) VALUES (?,?)`).run(req.body.email, hashed);
+       
+        const createdUser = db.prepare(`SELECT * FROM users WHERE ID = ? `).get(newUser.lastInsertRowid);
+
+        req.session.userId = createdUser.id
+        req.session.email = createdUser.email
+        res.status(201).json(createdUser)
+    } catch (error) {
+        console.error(error)
+        res.json(error)
+    }
+}
+)
+
+app.post("/auth/signin", (req, res) => {
+    
+}
+)
+app.listen("3000", () => {
+    console.log("Порт3000")
+})
+
